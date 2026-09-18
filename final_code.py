aa544af48d0a8157173b71f7605dc4a37d404ab9
Prithika <Prithika>
2026-09-18 12:05:57 +0530

    Add generated code from PROMPTEX

--- final_code.py
+++ final_code.py
@@ -1,41 +1,63 @@
-from collections import deque
+class Node:
+    def __init__(self, data):
+        """
+        Initialize a new Node object with given data, a previous pointer, and a next pointer.
+        """
+        self.data = data
+        self.prev = None
+        self.next = None
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+class DoublyLinkedList:
+    def __init__(self):
+        """
+        Initialize a new DoublyLinkedList object with a head and a tail set to None.
+        """
+        self.head = None
+        self.tail = None
 
-def bfs_traversal(root):
-    """
-    Perform a breadth-first traversal of a binary tree and return a list of lists,
-    where each inner list represents a level of the tree.
+    def insert(self, data):
+        """
+        Insert a new node with the given data into the DoublyLinkedList.
+        If the list is empty, the new node becomes both the head and the tail.
+        Otherwise, the new node is inserted at the end of the list.
+        """
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
-    Parameters:
-    root (TreeNode): The root node of the binary tree.
+    def delete(self, data):
+        """
+        Delete the first occurrence of the given data from the DoublyLinkedList.
+        If the data is not found, return False.
+        If the node to be deleted is the head or tail, update the head or tail accordingly.
+        """
+        current = self.head
+        while current:
+            if current.data == data:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self.tail = current.prev
+                return True
+            current = current.next
+        return False
 
-    Returns:
-    list of list of int: A list of lists, where each sublist contains the values of nodes
-                          at the corresponding level of the tree.
-    """
-    if not root:
-        return []
-
-    queue = deque([root])
-    result = []
-
-    while queue:
-        level_size = len(queue)
-        current_level = []
-
-        for _ in range(level_size):
-            node = queue.popleft()
-            current_level.append(node.val)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-
-        result.append(current_level)
-
-    return result
+    def search(self, data):
+        """
+        Search for the given data in the DoublyLinkedList.
+        Return True if the data is found, False otherwise.
+        """
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
